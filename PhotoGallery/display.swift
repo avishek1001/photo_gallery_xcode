2179f30c222eae105697b261e7990b48da942cb0
Avishek <1706avishek@gmail.com>
2023-11-01 12:37:18 -0700

    added slider functionality

--- PhotoGallery/display.swift
+++ PhotoGallery/display.swift
@@ -12,27 +12,61 @@ class display: UIViewController {
     @IBOutlet weak var img: UIImageView!
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         img.image = UIImage(named: cam[myIndex])
 
-        // Do any additional setup after loading the view.
+        home.layer.cornerRadius = 10
+        prevBtn.layer.cornerRadius = 10
+        nextBtn.layer.cornerRadius = 10
+        
+        img.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
     }
+    
+    @IBOutlet weak var home: UIButton!
+    @IBOutlet weak var prevBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    @IBOutlet weak var zoomSlider: UISlider!
     
     
     
     @IBAction func prevAction(_ sender: Any) {
+        myIndex -= 1
         
-        if myIndex > 0 {
-            img.image = UIImage(named: cam[myIndex - 1])
+        if myIndex < 0 {
+            myIndex  = cam.count - 1
         }
+        
+        img.image = UIImage(named: cam[myIndex])
     }
     
     @IBAction func nextAction(_ sender: Any) {
-        img.image = UIImage(named: cam[myIndex + 1])
+        myIndex += 1
+        
+        if myIndex >= cam.count {
+            myIndex = 0
+        }
+        img.image = UIImage(named: cam[myIndex])
     }
     
+    @IBAction func zoom(_ sender: Any) {
+        let zoomScale = CGFloat(zoomSlider.value)
+        img.transform = CGAffineTransform(scaleX: zoomScale, y: zoomScale)
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        zoomSlider.maximumValue = 2.0
+        zoomSlider.minimumValue = 0.5
+    }
     
 }
+    
+    
+    
+
+
+
