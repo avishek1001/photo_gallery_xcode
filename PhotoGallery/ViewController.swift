8f63be065e540bba90b4d7426106413c9c0abd68
Avishek <1706avishek@gmail.com>
2023-11-02 02:56:39 -0700

    alert

--- PhotoGallery/ViewController.swift
+++ PhotoGallery/ViewController.swift
@@ -14,14 +14,15 @@ class ViewController: UIViewController, UITextFieldDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        login.layer.cornerRadius = 20
-        
+        login.layer.cornerRadius = 38
+        reset.layer.cornerRadius = 40
         email.delegate = self
         password.delegate = self
     }
 
     @IBOutlet weak var login: UIButton!
     
+    @IBOutlet weak var reset: UIButton!
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
@@ -32,11 +33,29 @@ class ViewController: UIViewController, UITextFieldDelegate{
         if email.text == "admin" && password.text == "admin123"{
             performSegue(withIdentifier: "main", sender: nil)
         }
+        else {
+            let alrt = UIAlertController(title: "Wrong Password", message: "", preferredStyle: UIAlertController.Style.alert)
+            alrt.addAction(UIAlertAction(title: "Try Again", style: UIAlertAction.Style.destructive, handler: nil))
+            present(alrt, animated: true, completion: nil)
+            
+            email.text = ""
+            password.text = ""
+        }
+    }
+    
+    
+    @IBAction func clearAction(_ sender: Any) {
+        email.text = ""
+        password.text = ""
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 }
 
