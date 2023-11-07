2ce59873220c2602413d1bba6fa031979a223d65
Avishek <1706avishek@gmail.com>
2023-11-07 02:35:28 -0800

    made videoTable

---
+++ PhotoGallery/videoDisplay.swift
@@ -0,0 +1,31 @@
+//
+//  videoDisplay.swift
+//  PhotoGallery
+//
+//  Created by avishek on 07/11/23.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class videoDisplay: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
